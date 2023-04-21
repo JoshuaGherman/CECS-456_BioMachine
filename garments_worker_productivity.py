40a7e5ddfe054a329fb00ceab654c229f61b04e9
Ryo Fujimura <ryo176999@gmail.com>
2023-04-21 10:55:31 -0700

    1, 1-1, 1-2, 1-3

--- garments_worker_productivity.py
+++ garments_worker_productivity.py
@@ -1,47 +1,67 @@
+'''
+Tasks: Clustering student behavior based on their test scores 
+Instructions/Directions: 
+    Perform exploratory data analysis (EDA) using data visualization, for example, histogram of the features, boxplot, and apart from this you are encouraged to explore EDA and plot relevant graphs.   
+        1.1 Identify the outliers in the dataset   
+        1.2 Plot the correlation matrix for the dataset.   
+        1.3 Plot the graphical distribution for the variables   
+    Identify the optimal number of clusters in the dataset.
+        2.1.  You may want to compare silhouette and elbow method.   
+    Use k-means algorithm for creating the clusters.  
+        3.1 Interpret each of the clusters in question 3.  
+    Use HDBSCAN to perform hierarchical clustering and plot the dendrogram.  
+    Compare the results of various clustering algorithms. 
+'''
+
+import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from scipy.cluster.hierarchy import dendrogram
-import hdbscan
+
+# Create a directory to save the figures
+if not os.path.exists('figures'):
+    os.mkdir('figures')
 
 # Load the dataset
-df = pd.read_csv('garments_worker_productivity.csv')
+df = pd.read_csv('garments_worker_productivity.csv')  # Replace 'your_dataset.csv' with the actual filename or path of your dataset
 
-# Display the first few rows of the dataset
-df.head()
+# 1.1 Identify the outliers in the dataset
+# Plot boxplots for numerical features to identify outliers
+numerical_features = df.select_dtypes(include='number').columns
+for feature in numerical_features:
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(x=feature, data=df)
+    plt.title(f'Boxplot of {feature}')
+    plt.savefig(f'figures/boxplot_{feature}.png')
+    plt.close()
 
-# Perform EDA using data visualization
-
-# Histogram of the features
-df.hist(figsize=(10,8))
-plt.tight_layout()
-plt.show()
-
-# Boxplot for the variables
-plt.figure(figsize=(10,8))
-sns.boxplot(data=df)
-plt.title('Boxplot of Variables')
-plt.show()
-
-# Correlation matrix
+# 1.2 Plot the correlation matrix for the dataset
+# Calculate and plot the correlation matrix
 corr = df.corr()
-plt.figure(figsize=(10,8))
+plt.figure(figsize=(12, 8))
 sns.heatmap(corr, annot=True, cmap='coolwarm')
 plt.title('Correlation Matrix')
-plt.show()
+plt.savefig('figures/correlation_matrix.png')
+plt.close()
 
-# Pairplot of variables
-sns.pairplot(df, diag_kind='kde')
-plt.suptitle('Pairplot of Variables')
-plt.show()
+# 1.3 Plot the graphical distribution for the variables
+# Plot histograms for numerical features
+for feature in numerical_features:
+    plt.figure(figsize=(8, 6))
+    sns.histplot(df[feature], bins=20, kde=True)
+    plt.title(f'Histogram of {feature}')
+    plt.xlabel(feature)
+    plt.ylabel('Frequency')
+    plt.savefig(f'figures/histogram_{feature}.png')
+    plt.close()
 
-# Identify outliers
-
-# Z-score method
-from scipy.stats import zscore
-z_scores = zscore(df)
-outliers = np.where((z_scores > 3) | (z_scores < -3))
-outliers
+# Plot countplots for categorical features
+categorical_features = df.select_dtypes(include='object').columns
+for feature in categorical_features:
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x=feature, data=df)
+    plt.title(f'Countplot of {feature}')
+    plt.xlabel(feature)
+    plt.ylabel('Count')
+    plt.savefig(f'figures/countplot_{feature}.png')
+    plt.close()
