7e45c954127d650101163408a8daf7eef5c85da6
JasonJ-CSULB <80545301+JasonJ-CSULB@users.noreply.github.com>
2023-04-25 19:25:23 -0700

    Added Elbow and Silhouette Method
    Added Elbow and Silhouette Method to find clusters

--- cecs_456_final_project.py
+++ cecs_456_final_project.py
@@ -33,3 +33,33 @@ corrMatrix = dataset.corr()
 # Plotting Correlation Matrix
 plt.matshow(corrMatrix)
 plt.show()
+
+#Finding Optimal Number of Clusters
+#Elbow Method
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from yellowbrick.cluster import KElbowVisualizer
+
+km = KMeans(random_state=42)
+visualizer = KElbowVisualizer(km, k=(2,10))
+ 
+visualizer.fit(X)
+visualizer.show()
+
+#Sillouette
+from yellowbrick.cluster import SilhouetteVisualizer
+
+fig, ax = plt.subplots(3, 2, figsize=(15,8))
+for i in [2, 3, 4, 5, 6, 7]:
+    '''
+    Create KMeans instance for different number of clusters
+    '''
+    km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=100, random_state=42)
+    q, mod = divmod(i, 2)
+    '''
+    Create SilhouetteVisualizer instance with KMeans instance
+    Fit the visualizer
+    '''
+    visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[q-1][mod])
+    visualizer.fit(X)
+
