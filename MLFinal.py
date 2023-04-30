0ad9779496ad99ad39713cb1eb72c57ba94509c5
JasonJ-CSULB <80545301+JasonJ-CSULB@users.noreply.github.com>
2023-04-29 18:45:15 -0700

    Updated student_data and MLFinal
    Added EDA analysis and more visuals to the new MLFinal and updated student_data so there are no students without a final grade

--- MLFinal.py
+++ MLFinal.py
@@ -18,6 +18,7 @@ import seaborn as sn
 # Ingesting custom csv of data
 dataset = pd.read_csv('student_data.csv', delimiter=',')
 dataset[np.isnan(dataset)] = 0
+
 # Breaking dataframe into potentially useful subsects
 finalGrades = dataset[['StudentID','fg1', 'fg2']]
 sessionGrades = dataset[['StudentID', 'sg2', 'sg3', 'sg4', 'sg5', 'sg6']]
@@ -56,6 +57,7 @@ plt.xlabel('Number of Sessions Attended')
 plt.ylabel('Number of Students')
 plt.show()
 '''
+
 # Adding attendance to dataframe
 dataset['Attendance'] = attList
 
@@ -67,6 +69,18 @@ for i, row in dataset.iterrows():
     else:
         bestFinalScore.append(row['fg2'])
 dataset['BestFinalScore'] = bestFinalScore
+
+#setting up x and y for more EDA analysis
+X = dataset.iloc[:, :-1].values
+y = bestFinalScore
+
+#boxplot of grades
+plt.boxplot(y) #shows the score of the students' grades as a boxplot.
+plt.show()
+
+#Histogram of the number of grades with a specific score.
+plt.hist(y)
+plt.show()
 
 # Creating Correlation Matrix
 corrMatrix = dataset.corr()
@@ -81,6 +95,7 @@ corrMatrix = dataRel.corr()
 # Visulization of Correlation Matrix
 sn.heatmap(corrMatrix, annot=True)
 plt.show()
+
 '''
 # Distribution of 1st Final Attempts
 # Binwidth set to 10 to show typical 10 percent grading bands
@@ -99,18 +114,6 @@ sn.displot(dataset, x='sg5', binwidth=0.5)
 sn.displot(dataset, x='sg6', binwidth=0.5)
 plt.show()
 '''
-##############################################################################################
-# At least some of the outliers in the dataset are the students that only attended
-# the first session. They will skew the distribution down
-
-from sklearn.decomposition import PCA
-pca = PCA(2)
-data = pca.fit_transform(dataset)
-data.shape
-
-# Another test
-data2 = pca.fit_transform(dataRel)
-data2.shape
 
 # Identifying optimal num of clusters for K-means clustering using:
 # Elbow Method
@@ -135,9 +138,22 @@ plt.title('The Elbow Method showing the optimal k')
 plt.show()
 # The Elbow method indicates that 3 or 4 is the optimal num of clusters
 
+#Alternative Elbow Method
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from yellowbrick.cluster import KElbowVisualizer
+
+km = KMeans(random_state=42)
+visualizer = KElbowVisualizer(km, k=(2,10))
+ 
+visualizer.fit(X)
+visualizer.show()
+#This Elbow method indicates 4 is optimal, so we need to check both 3 and 4 since 3 is optimal in the elbow method above.
+
 # Silhouette Method
 from sklearn.metrics import silhouette_score
 
+silhouettesVal = [] #used to make the graph at the end
 range_n_clusters = list(range(2,10))
 for n_clusters in range_n_clusters:
     clusterer = KMeans(n_clusters=n_clusters, n_init=10)
@@ -145,9 +161,33 @@ for n_clusters in range_n_clusters:
     centers = clusterer.cluster_centers_
 
     score = silhouette_score(dataVals, preds)
+    silhouettesVal.append(score)
     print("For n_clusters = {}, silhouette score is {})".format(n_clusters, score))
-# The Silhouette methods indicates that 3 or 4 is the optimal num of clusters
+# The Silhouette methods indicates that 2 is the optimal num of clusters
 
+plt.plot(range_n_clusters, silhouettesVal,'bx-')
+plt.xlabel('Values of K') 
+plt.ylabel('Silhouette score') 
+plt.title('Silhouette analysis For Optimal k')
+plt.show()
+
+#Another visual of silhouette method
+from yellowbrick.cluster import SilhouetteVisualizer
+
+fig, ax = plt.subplots(3, 2, figsize=(15,8))
+for i in [2, 3, 4, 5, 6, 7,]:
+    #Create KMeans instance for different number of clusters
+
+    km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=100, random_state=42)
+    q, mod = divmod(i, 2)
+   
+    # Create SilhouetteVisualizer instance with KMeans instance and Fit the visualizer
+
+    visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[q-1][mod])
+    visualizer.fit(X)
+#2 clusters is shown to be the best here since the other ones are way off.
+
+#Clustering attempts
 # With these results lets use 4 clusters
 kmeans = KMeans(n_clusters=4, n_init=10)
 label = kmeans.fit_predict(dataVals)
@@ -165,7 +205,40 @@ plt.show()
 # final attempt, the k-means graph shows a strong connection between a student's grade 
 # and their class attendance
 
-#Clustering attempts
+# With these results lets use 3 clusters
+kmeans = KMeans(n_clusters=3, n_init=10)
+label = kmeans.fit_predict(dataVals)
+# Getting Centroids
+centroids = kmeans.cluster_centers_
+# Getting unique labels
+u_labels = np.unique(label)
+# plotting the results
+for i in u_labels:
+    plt.scatter(dataVals[label == i, 0] , dataVals[label == i, 1] , label = i)
+plt.scatter(centroids[:,0] , centroids[:,1] , s = 20, color = 'k')
+plt.legend()
+plt.show()
+# Utilizing a focused subset of the data, frequency of student attendance and highest
+# final attempt, the k-means graph shows a strong connection between a student's grade 
+# and their class attendance
+
+# With these results lets use 2 clusters
+kmeans = KMeans(n_clusters=2, n_init=10)
+label = kmeans.fit_predict(dataVals)
+# Getting Centroids
+centroids = kmeans.cluster_centers_
+# Getting unique labels
+u_labels = np.unique(label)
+# plotting the results
+for i in u_labels:
+    plt.scatter(dataVals[label == i, 0] , dataVals[label == i, 1] , label = i)
+plt.scatter(centroids[:,0] , centroids[:,1] , s = 20, color = 'k')
+plt.legend()
+plt.show()
+# Utilizing a focused subset of the data, frequency of student attendance and highest
+# final attempt, the k-means graph shows a strong connection between a student's grade 
+# and their class attendance
+
 #using HDBSCAN
 import hdbscan
 HDB_cluster = hdbscan.HDBSCAN(min_cluster_size=5)
